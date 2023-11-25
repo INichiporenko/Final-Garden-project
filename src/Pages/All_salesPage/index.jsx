4d5c03172efc9b78f2b8dea4d643540dd4bc733f
Nychyporenko <i.v.nichiporenko25@gmail.com>
2023-11-25 19:01:53 +0100

    first commit

---
+++ src/Pages/All_salesPage/index.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { useSelector } from 'react-redux';
+import ProductsFilter from '../../Components/ProductsFilter';
+import ProductsSalesItem from '../../Components/ProductsSalesItem';
+import Conteiner from '../../UI/Container';
+import s from './style.module.css';
+
+
+
+export default function All_salesPage() {
+
+  const {list} = useSelector((state) => state.products);
+
+  const filteredProducts = list.filter(product => product.discont_price !== null);
+
+  return (
+    <div className={s.block}>
+      <Conteiner className={s.products} >
+        <p className={s.text} >Products with sale</p>
+        <ProductsFilter showDiscountedCheckbox={false}/>
+        <div className={s.map}>
+          {
+            filteredProducts
+            .filter(({show}) => Object.values(show).every(elem => elem))
+            .map(product => (
+            <ProductsSalesItem key={product.id} {...product} />))
+          }
+        </div>
+      </Conteiner>
+    </div>
+  )
+}
